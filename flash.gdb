8c7ed7af0aa8648e8628cfea46a0e04c5826febb
Ben Clarke <ben@arrayofstars.com>
2022-12-21 13:02:22 -0500

    Document and clean up working build process

---
+++ flash.gdb
@@ -0,0 +1,4 @@
+target extended-remote localhost:4242
+load
+disconnect
+quit
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,26 @@ use panic_halt as _;
 
 use stm32f0xx_hal as hal;
 
-use crate::hal::{delay::Delay, pac, prelude::*};
-
-use cortex_m::peripheral::Peripherals;
+use hal::{delay::Delay, pac, prelude::*};
+use cortex_m::{ interrupt::free, peripheral::Peripherals };
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
     if let (Some(mut p), Some(cp)) = (pac::Peripherals::take(), Peripherals::take()) {
         let mut rcc = p.RCC.configure().sysclk(48.mhz()).freeze(&mut p.FLASH);
+        let gpioa = p.GPIOA.split(&mut rcc);
         let gpiob = p.GPIOB.split(&mut rcc);
-        let mut led = cortex_m::interrupt::free(|cs| gpiob.pb3.into_push_pull_output(cs));
+        let mut led = free(|cs| gpiob.pb3.into_push_pull_output(cs));
+        let mut led2 = free(|cs| gpioa.pa1.into_push_pull_output(cs));
         let mut delay = Delay::new(cp.SYST, &rcc);
 
+        led2.toggle().ok();
+
         loop {
-            led.toggle().ok();
             delay.delay_ms(2000_u16);
+            led.toggle().ok();
+            led2.toggle().ok();
         }
     }
 
